0574d2154469f3a002e34c17139b4542451427c5
Scott Frazer <frazer.scott@gmail.com>
2011-08-09 10:02:58 -0400

    Make WmConfig purely config data, no Xlib data

--- WmConfig.py
+++ WmConfig.py
@@ -2,16 +2,12 @@
 
 import sys
 import logging
+import types
 import ConfigParser
-import Xlib.X as X
-import Xlib.XK as XK
 
 ################################################################################
 
 class WmConfigObject(object):
-
-    def __init__(self, display):
-        self.display = display
 
     def parse(self, config_obj, section):
 
@@ -19,37 +15,31 @@ class WmConfigObject(object):
             return
 
         for option in config_obj.options(section):
+
             if not hasattr(self, option):
                 print "Unknown option '%s' in '%s' section" % (option, section)
                 sys.exit(1)
-            setattr(self, option, config_obj.get(section, option))
 
-        self.init_after_parse()
-
-    def init_after_parse(self):
-        pass
+            if type(getattr(self, option)) == types.IntType:
+                setattr(self, option, int(config_obj.get(section, option)))
+            else:
+                setattr(self, option, config_obj.get(section, option))
 
 ################################################################################
 
 class WmConfigStack(WmConfigObject):
 
-    def __init__(self, display):
+    def __init__(self, config_obj, section):
 
-        super(WmConfigStack, self).__init__(display)
         self.border_width = 1
 
-    def init_after_parse(self):
-
-        super(WmConfigStack, self).init_after_parse()
-        self.border_width = int(self.border_width)
+        self.parse(config_obj, section)
 
 ################################################################################
 
 class WmConfigTab(WmConfigObject):
 
-    def __init__(self, display):
-
-        super(WmConfigTab, self).__init__(display)
+    def __init__(self, config_obj, section):
 
         self.font = '-*-helvetica-medium-r-*-*-12-*'
         self.border_width = 1
@@ -71,60 +61,26 @@ class WmConfigTab(WmConfigObject):
         self.uu_bg = '#304050'
         self.uu_bo = '#506070'
 
-    def init_after_parse(self):
-
-        super(WmConfigTab, self).init_after_parse()
-
-        self.border_width = int(self.border_width)
-        self.padding = int(self.padding)
-
-        colormap = self.display.screen(0).default_colormap
-
-        self.ff_fg = colormap.alloc_named_color(self.ff_fg).pixel
-        self.ff_bg = colormap.alloc_named_color(self.ff_bg).pixel
-        self.ff_bo = colormap.alloc_named_color(self.ff_bo).pixel
-
-        self.fu_fg = colormap.alloc_named_color(self.fu_fg).pixel
-        self.fu_bg = colormap.alloc_named_color(self.fu_bg).pixel
-        self.fu_bo = colormap.alloc_named_color(self.fu_bo).pixel
-
-        self.uf_fg = colormap.alloc_named_color(self.uf_fg).pixel
-        self.uf_bg = colormap.alloc_named_color(self.uf_bg).pixel
-        self.uf_bo = colormap.alloc_named_color(self.uf_bo).pixel
-
-        self.uu_fg = colormap.alloc_named_color(self.uu_fg).pixel
-        self.uu_bg = colormap.alloc_named_color(self.uu_bg).pixel
-        self.uu_bo = colormap.alloc_named_color(self.uu_bo).pixel
+        self.parse(config_obj, section)
 
 ################################################################################
 
 class WmConfigStatusBar(WmConfigObject):
 
-    def __init__(self, display):
+    def __init__(self, config_obj, section):
 
-        super(WmConfigStatusBar, self).__init__(display)
         self.border_width = 1
 
         self.bg = '#607080'
         self.bo = '#607080'
 
-    def init_after_parse(self):
-
-        super(WmConfigStatusBar, self).init_after_parse()
-        self.border_width = int(self.border_width)
-
-        colormap = self.display.screen(0).default_colormap
-
-        self.bg = colormap.alloc_named_color(self.bg).pixel
-        self.bo = colormap.alloc_named_color(self.bo).pixel
+        self.parse(config_obj, section)
 
 ################################################################################
 
 class WmConfigGroup(WmConfigObject):
 
-    def __init__(self, display):
-
-        super(WmConfigGroup, self).__init__(display)
+    def __init__(self, config_obj, section):
 
         self.font = '-*-helvetica-medium-r-*-*-14-*'
         self.border_width = 1
@@ -138,30 +94,13 @@ class WmConfigGroup(WmConfigObject):
         self.inactive_bg = '#405060'
         self.inactive_bo = '#607080'
 
-    def init_after_parse(self):
-
-        super(WmConfigGroup, self).init_after_parse()
-
-        self.border_width = int(self.border_width)
-        self.padding = int(self.padding)
-
-        colormap = self.display.screen(0).default_colormap
-
-        self.active_fg = colormap.alloc_named_color(self.active_fg).pixel
-        self.active_bg = colormap.alloc_named_color(self.active_bg).pixel
-        self.active_bo = colormap.alloc_named_color(self.active_bo).pixel
-
-        self.inactive_fg = colormap.alloc_named_color(self.inactive_fg).pixel
-        self.inactive_bg = colormap.alloc_named_color(self.inactive_bg).pixel
-        self.inactive_bo = colormap.alloc_named_color(self.inactive_bo).pixel
+        self.parse(config_obj, section)
 
 ################################################################################
 
 class WmConfigMeter(WmConfigObject):
 
-    def __init__(self, display):
-
-        super(WmConfigMeter, self).__init__(display)
+    def __init__(self, config_obj, section):
 
         self.font = '-*-helvetica-medium-r-*-*-14-*'
         self.border_width = 1
@@ -171,44 +110,34 @@ class WmConfigMeter(WmConfigObject):
         self.bg = '#607080'
         self.bo = '#708090'
 
-    def init_after_parse(self):
+        self.parse(config_obj, section)
 
-        super(WmConfigMeter, self).init_after_parse()
+################################################################################
 
-        self.border_width = int(self.border_width)
-        self.padding = int(self.padding)
+class WmConfigKeys(WmConfigObject):
 
-        colormap = self.display.screen(0).default_colormap
+    def __init__(self, config_obj, section):
 
-        self.fg = colormap.alloc_named_color(self.fg).pixel
-        self.bg = colormap.alloc_named_color(self.bg).pixel
-        self.bo = colormap.alloc_named_color(self.bo).pixel
+        self.next_window = "Mod4+n"
+        self.prev_window = "Mod4+p"
+        self.kill_window = "Mod4+q"
+
+        self.parse(config_obj, section)
 
 ################################################################################
 
 class WmConfig(object):
 
-    def __init__(self, display):
+    def __init__(self):
 
         config_obj = self.read_config_file()
 
-        self.stack = WmConfigStack(display)
-        self.stack.parse(config_obj, "Stack")
-
-        self.tab = WmConfigTab(display)
-        self.tab.parse(config_obj, "Tab")
-
-        self.status_bar = WmConfigStatusBar(display)
-        self.status_bar.parse(config_obj, "StatusBar")
-
-        self.group = WmConfigGroup(display)
-        self.group.parse(config_obj, "Group")
-
-        self.meter = WmConfigMeter(display)
-        self.meter.parse(config_obj, "Meter")
-
-        self.keys = {}
-        self.parse_keys(display, config_obj)
+        self.stack = WmConfigStack(config_obj, "Stack")
+        self.tab = WmConfigTab(config_obj, "Tab")
+        self.status_bar = WmConfigStatusBar(config_obj, "StatusBar")
+        self.group = WmConfigGroup(config_obj, "Group")
+        self.meter = WmConfigMeter(config_obj, "Meter")
+        self.keys = WmConfigKeys(config_obj, "Keys")
 
     ############################################################################
 
@@ -228,42 +157,3 @@ class WmConfig(object):
         config_file.close()
 
         return config
-
-    ############################################################################
-
-    def parse_keys(self, display, config):
-
-        section = 'Keys'
-
-        if section not in config.sections():
-            return
-
-        commands = ['next_window',
-                    'prev_window',
-                    'kill_window']
-
-        # TODO Handle multiple modifiers
-
-        for key_def in config.options(section):
-
-            (modifier, key) = key_def.split('+')
-
-            if modifier == 'mod4':
-                mod_mask = X.Mod4Mask
-            elif modifier == 'mod3':
-                mod_mask = X.Mod3Mask
-            elif modifier == 'mod2':
-                mod_mask = X.Mod2Mask
-            elif modifier == 'mod1':
-                mod_mask = X.Mod1Mask
-            else:
-                print "%s?" % (modifier)
-                sys.exit(1)
-
-            cmd = config.get(section, key_def)
-            if cmd not in commands:
-                print "Unknown command '%s' in '%s' section" % (cmd, section)
-                sys.exit(1)
-
-            keycode = display.keysym_to_keycode(XK.string_to_keysym(key))
-            self.keys[(mod_mask, keycode)] = cmd
--- WmData.py
+++ WmData.py
@@ -1,5 +1,8 @@
 # WmData.py
 
+import Xlib.X as X
+import Xlib.XK as XK
+
 class WmDataGC(object):
     pass
 
@@ -24,6 +27,75 @@ class WmData(object):
         self.setup_atoms()
 
     ############################################################################
+
+    def parse_keys(self, display, config):
+
+        section = 'Keys'
+
+        if section not in config.sections():
+            return
+
+        commands = ['next_window',
+                    'prev_window',
+                    'kill_window']
+
+        # TODO Handle multiple modifiers
+
+        for key_def in config.options(section):
+
+            (modifier, key) = key_def.split('+')
+
+            if modifier == 'mod4':
+                mod_mask = X.Mod4Mask
+            elif modifier == 'mod3':
+                mod_mask = X.Mod3Mask
+            elif modifier == 'mod2':
+                mod_mask = X.Mod2Mask
+            elif modifier == 'mod1':
+                mod_mask = X.Mod1Mask
+            else:
+                print "%s?" % (modifier)
+                sys.exit(1)
+
+            cmd = config.get(section, key_def)
+            if cmd not in commands:
+                print "Unknown command '%s' in '%s' section" % (cmd, section)
+                sys.exit(1)
+
+            keycode = display.keysym_to_keycode(XK.string_to_keysym(key))
+            self.keys[(mod_mask, keycode)] = cmd
+        colormap = self.display.screen(0).default_colormap
+
+        self.ff_fg = colormap.alloc_named_color(self.ff_fg).pixel
+        self.ff_bg = colormap.alloc_named_color(self.ff_bg).pixel
+        self.ff_bo = colormap.alloc_named_color(self.ff_bo).pixel
+
+        self.fu_fg = colormap.alloc_named_color(self.fu_fg).pixel
+        self.fu_bg = colormap.alloc_named_color(self.fu_bg).pixel
+        self.fu_bo = colormap.alloc_named_color(self.fu_bo).pixel
+
+        self.uf_fg = colormap.alloc_named_color(self.uf_fg).pixel
+        self.uf_bg = colormap.alloc_named_color(self.uf_bg).pixel
+        self.uf_bo = colormap.alloc_named_color(self.uf_bo).pixel
+
+        self.uu_fg = colormap.alloc_named_color(self.uu_fg).pixel
+        self.uu_bg = colormap.alloc_named_color(self.uu_bg).pixel
+        self.uu_bo = colormap.alloc_named_color(self.uu_bo).pixel
+
+        self.bg = colormap.alloc_named_color(self.bg).pixel
+        self.bo = colormap.alloc_named_color(self.bo).pixel
+
+        self.active_fg = colormap.alloc_named_color(self.active_fg).pixel
+        self.active_bg = colormap.alloc_named_color(self.active_bg).pixel
+        self.active_bo = colormap.alloc_named_color(self.active_bo).pixel
+
+        self.inactive_fg = colormap.alloc_named_color(self.inactive_fg).pixel
+        self.inactive_bg = colormap.alloc_named_color(self.inactive_bg).pixel
+        self.inactive_bo = colormap.alloc_named_color(self.inactive_bo).pixel
+
+        self.fg = colormap.alloc_named_color(self.fg).pixel
+        self.bg = colormap.alloc_named_color(self.bg).pixel
+        self.bo = colormap.alloc_named_color(self.bo).pixel
 
     def setup_metrics_and_gcs(self):
 
